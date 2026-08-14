61d79c2c73e710a053e3c0023672f0fdd5978811
Shivam Shukla <shivam4tech@gmail.com>
2026-08-14 21:15:42 +0530

    release v1.4.0: zoom-robust overlay, dpr-tracked drag box, frozen drag geometry, persistent PDF results panel

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,5 +1,5 @@
 export const EXTENSION_NAME = 'QuickCopy';
-export const EXTENSION_VERSION = '1.0.0';
+export const EXTENSION_VERSION = '1.4.0';
 
 export const STORAGE_KEYS = {
   SETTINGS: 'quickcopy:settings',
